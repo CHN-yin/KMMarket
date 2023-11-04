c615f15b6200d11cf28dcdf59849ade9d3e76e87
CHN-yin <shenghong2003.@gmail.com>
2023-11-04 14:45:54 +0800

    chore

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { register } from 'swiper/element/bundle'
 import router from './router'
 import store from './store'
 import './style/index.scss'
+import VConsole from 'vconsole'
+
+const vconsole = new VConsole()
 
 const app = createApp(App)
-app.use(store).use(router).use(register).mount('#app')
+app.use(store).use(router).use(register).use(vconsole).mount('#app')
