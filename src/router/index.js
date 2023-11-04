a1da8282816493a191489b618f0035914a09a2f3
CHN-yin <shenghong2003.@gmail.com>
2023-11-04 14:17:44 +0800

    style

--- src/router/index.js
+++ src/router/index.js
@@ -74,14 +74,6 @@ const routes = [
     name: 'ChangePwd',
     component: () => import(/* webpackChunkName: "forget" */ '@/views/forget/ChangePwd')
   }
-//   {
-//     path: '/about',
-//     name: 'about',
-//     // route level code-splitting
-//     // this generates a separate chunk (about.[hash].js) for this route
-//     // which is lazy-loaded when the route is visited.
-//     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-//   }
 ]
 
 const router = createRouter({
